955c6cf9b29b0b5fe17ef4a222f2f495adcef515
shivaani1009 <sshiva1009@gmail.com>
2025-06-02 10:02:45 +0800

    Initial commit

---
+++ cafe-service/src/main/java/com/example/cafe_service/CafeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafe_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeServiceApplication.class, args);
+	}
+
+}
---
+++ cafe-service/src/test/java/com/example/cafe_service/CafeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafe_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
